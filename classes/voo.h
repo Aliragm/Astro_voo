e1213ff88bfb66db65127c06872fbdb0559a78e9
André Lira <140771656+Aliragm@users.noreply.github.com>
2024-04-23 14:55:48 -0300

    Update voo.h

--- classes/voo.h
+++ classes/voo.h
@@ -1,16 +1,22 @@
+#ifndef VOO_H
+#define VOO_H
+
 #include "astronauta.h"
 #include <list>
 
-class voo{
+class voo {
 private:
-	bool voando;
-	bool explodiu;
-	int codigo;
-	int qntd_tripulantes;
-	std::list<*astronauta> tripulantes;
+    bool voando;
+    bool explodiu;
+    int codigo;
+    int qntd_tripulantes;
+    std::list<astronauta*> tripulantes;
+
 public:
-	voo(int codigo);
-	~voo();
-	void explodir();
-	void addtripulante(astronauta &tripulante);
+    voo(int codigo);
+    ~voo();
+    void explodir();
+    void addtripulante(astronauta *tripulante);
 };
+
+#endif
