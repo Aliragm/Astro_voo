3e9e6e35ade1b174c2cc4c900e5f0e0c686a46f4
André Lira <140771656+Aliragm@users.noreply.github.com>
2024-04-23 14:55:06 -0300

    Update astronauta.h

--- classes/astronauta.h
+++ classes/astronauta.h
@@ -1,20 +1,25 @@
+#ifndef ASTRONAUTA_H
+#define ASTRONAUTA_H
+
 #include <string>
 #include <iostream>
 #include <list>
 
 class astronauta{
 private:
-	std::string cpf;
-	std::string nome;
-	int idade;
-	bool esta_vivo;
-	bool esta_disponivel;
-	int qntd_voos;
-	std::list<int> voos;
+    std::string cpf;
+    std::string nome;
+    int idade;
+    bool esta_vivo;
+    bool esta_disponivel;
+    int qntd_voos;
+    std::list<int> voos;
 public:
-	astronauta(std::string cpf, std::string nome, int idade);
-	~astronauta();
-	void morte();
-	void voou(int cod);
-	
+    astronauta(std::string cpf, std::string nome, int idade);
+    ~astronauta();
+    void morte();
+    void voou(int cod);
+    
 };
+
+#endif
