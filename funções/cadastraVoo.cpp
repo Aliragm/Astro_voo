33ee615d1f255f41030cf55cfe78d0bc90a94c19
André Lira <140771656+Aliragm@users.noreply.github.com>
2024-04-24 21:13:00 -0300

    Update cadastraVoo.cpp

--- funções/cadastraVoo.cpp
+++ funções/cadastraVoo.cpp
@@ -1,9 +1,10 @@
 #include "cadastraVoo.h"
 
-void cadastra_voo(){
+void cadastro_voo(gerenciador a){
 	int codigo;
 	std::cout << "Digite o codigo do voo: " << std::endl;
 	std::cout << ":";
 	std::cin >> codigo;
-	voo a(codigo);
+	voo b(codigo);
+	a.voo_criado(&b);
 }
