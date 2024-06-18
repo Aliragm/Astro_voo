589f174318656e70eec773402e4c8c60467ed946
André Lira <140771656+Aliragm@users.noreply.github.com>
2024-06-18 14:25:46 -0300

    Update gerenciador.h

--- classes/gerenciador.h
+++ classes/gerenciador.h
@@ -6,14 +6,19 @@
 #include <map>
 
 class gerenciador {
-private:
-	static std::map<int, astronauta*> astronautas;
-	static std::list<int> voos;
-	static int qntd_astros;
 public:
+	static std::map<int, astronauta*> astronautas;
+	static std::map<int, voo*> voos;
+	static int qntd_astros;
+	static int qntd_voos;
+	
 	~gerenciador();
 	void astronauta_criado(astronauta* tripulante);
 	void voo_criado(voo* espaconave);
+	void exibir_astros_vivos();
+	void exibir_voos_aguardando();
+	void exibir_voos_com_astronautas();
+	bool checar_voos_com_astronautas();
 };
 
 #endif
