782761fcf95331b46d689a4e511eea1aaea32bc9
André Lira <140771656+Aliragm@users.noreply.github.com>
2024-06-18 14:29:28 -0300

    Update main.cpp

--- funções/main.cpp
+++ funções/main.cpp
@@ -4,21 +4,29 @@
 #include "cadastraVoo.h"
 #include "logo.h"
 #include "gerenciador.h"
+#include "atribuir_astronauta.h"
+#include "desatribuir_astronauta.h"
 
 int main(){
 	gerenciador a;
 	logo();
 	while(true){
-		int escolha = 0;
+		int escolha = 999;
+		std::cout << std::endl;
 		std::cout << "Digite o que deseja fazer: " << std::endl;
 		std::cout << "0 - fechar o programa" << std::endl;
 		std::cout << "1 - cadastro astronauta" << std::endl;
 		std::cout << "2 - criar voo" << std::endl;
+		std::cout << "3 - atribuir astronauta a voo" << std::endl;
+		std::cout << "4 - desatribuir astronauta a voo" << std::endl;
 		std::cin >> escolha;
+		std::cout << std::endl;
 		switch(escolha){
 		case 0: return 0;
-		case 1: cadastro_astro(a);
-		case 2: cadastro_voo(a);
+		case 1: cadastro_astro(a);std::cin.ignore();break;
+		case 2: cadastro_voo(a);std::cin.ignore();break;
+		case 3: atribuicao(a);break;
+		case 4: desatribuir(a);break;
 		}
 	}
 
