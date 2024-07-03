94a17b23e6494f0e60e8ccb7e5c65c5e1b82d75d
André Lira <140771656+Aliragm@users.noreply.github.com>
2024-07-03 16:32:42 -0300

    Update explodir.cpp

--- funções/explodir.cpp
+++ funções/explodir.cpp
@@ -3,9 +3,14 @@
 void explodira(gerenciador &a){
 	int voo_escolhido;
 	std::cout << "os voos voando são os seguintes. Escolha (digite o indice): " << std::endl;
-	a.exibir_voos_voando();
-	std::cout << "\n:";
-    std::cin >> voo_escolhido;
-    voo* b = a.voos[voo_escolhido];
-    a.explodir_voo(b, voo_escolhido);
+	bool rumo = a.exibir_voos_voando();
+	if(rumo == true){
+		std::cout << "\n:";
+	    std::cin >> voo_escolhido;
+	    voo* b = a.voos[voo_escolhido];
+	    a.explodir_voo(b, voo_escolhido);
+	}
+	else{
+		return;
+	}
 }
